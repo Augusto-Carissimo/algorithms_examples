b0e35600892fbc1ff22e1e97fcc6481e5a357553
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-12-06 12:45:02 -0300

    Add LinkedList.insert

--- linked_list.rb
+++ linked_list.rb
@@ -1,16 +1,100 @@
 class Node
   attr_accessor :data, :next_node
 
-  def initialize(data)
+  def initialize(data, next_node = nil)
     @data = data
-    @next_node = nil
+    @next_node = next_node
   end
 
-  def repr
-    "<Node data: #{self.data[:data]}>"
+  def __repr__
+    "<Node data: #{self.data}>"
   end
 end
 
-node = Node.new(data: 10)
+class LinkedList
+  attr_accessor :head
 
-p node.repr
+  def head
+    @head if @head
+  end
+
+  def is_empty?
+    @head == nil
+  end
+
+  def size
+    current = @head
+    count = 0
+
+    while current
+      count += 1
+      current = current.next_node
+    end
+    count
+  end
+
+  def add(data)
+    new_node = Node.new(data, @head)
+    self.head= new_node
+    __repr__
+  end
+
+  def search(data)
+    current = @head
+    while current
+      if current.data == data
+        return current
+      else
+        current = current.next_node
+      end
+    end
+    return 'Not found'
+  end
+
+  def insert(data, index)
+    if index > self.size
+      return 'Index is out of range'
+    end
+
+    if index == 0
+      self.add(data)
+    end
+
+    if index > 0
+      new_node = Node.new(data)
+      position = index
+      current = @head
+
+      while position > 1
+        current = current.next_node
+        position -= 1
+      end
+
+      prev_node = current
+      next_node = current.next_node
+
+      prev_node.next_node = new_node
+      new_node.next_node = next_node
+    end
+
+    __repr__
+  end
+
+  def __repr__
+    nodes = []
+    current = @head
+
+    while current
+      if current == @head
+        nodes << "[Head: #{current.data}]"
+      elsif current.next_node.nil?
+        nodes << "[Tail: #{current.data}]"
+      else
+        nodes << "[#{current.data}]"
+      end
+      current = current.next_node
+    end
+
+    nodes.join('~>')
+  end
+end
