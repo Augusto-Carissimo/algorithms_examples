cc1ec29228ab4c9ee4b4bd4394b6649d38e3edb5
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-12-18 15:52:38 -0300

    Refactor LinkedListMergeSort

--- linked_list_merge_sort.rb
+++ linked_list_merge_sort.rb
@@ -16,12 +16,45 @@ def linked_list_merge_sort(linked_list)
       right_half = LinkedList.new
       right_half.head = mid_node.next_node
       mid_node.next_node = nil
+
       return left_half, right_half
     end
   end
 
   def conquer(left, right)
+    merged = LinkedList.new
 
+    merged.add(0)
+    current = merged.head
+
+    left_head = left.head
+    right_head = right.head
+
+    while left_head || right_head
+      if left_head.nil?
+        current.next_node = right_head
+        right_head  = right_head.next_node
+      elsif right_head.nil?
+        current.next_node = left_head
+        left_head = left_head.next_node
+      else
+        left_data = left_head.data
+        right_data = right_head.data
+        if left_data < right_data
+          current.next_node = left_head
+          left_head = left_head.next_node
+        else
+          current.next_node = right_head
+          right_head = right_head.next_node
+        end
+      end
+      current = current.next_node
+    end
+
+    head = merged.head.next_node
+    merged.head = head
+
+    return merged
   end
 
   if linked_list.size == 1 ||linked_list.head.nil?
@@ -29,16 +62,17 @@ def linked_list_merge_sort(linked_list)
   end
 
   left_half, right_half = divide(linked_list)
-  # left = linked_list_merge_sort(left_half)
-  # right = linked_list_merge_sort(right_half)
+  left = linked_list_merge_sort(left_half)
+  right = linked_list_merge_sort(right_half)
 
-  # return conquer(left, right)
+  return conquer(left, right)
 end
+
 l = LinkedList.new
-l.add(50)
-l.add(40)
+l.add(10)
 l.add(30)
-l.add(20)
+l.add(40)
+l.add(60)
 p l.__repr__
 
-p linked_list_merge_sort(l)
+p linked_list_merge_sort(l).__repr__
